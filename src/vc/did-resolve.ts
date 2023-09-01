c5da9f3a5833276ca86b5909c5763f355f44f549
Sunny <wnimlaor@nextfaze.com>
2023-09-01 13:46:10 +0930

    Update commnet

--- src/vc/did-resolve.ts
+++ src/vc/did-resolve.ts
@@ -6,7 +6,7 @@ const HEDERA_RESOLVER_HOST =
 /**
  * Placeholder did resolver.
  * Since universal resolvers don't yet typically support Hedera DIDs, assumes a
- * universal did resolver running at localhost:5000 that will resolve Hedera DIDs
+ * universal did resolver running at localhost:5000 that will resolve Hedera DIDs and other DIDs
  */
 export const resolveDidDocument = (did: string) =>
   fetchJson(`${HEDERA_RESOLVER_HOST}/${did}`);
